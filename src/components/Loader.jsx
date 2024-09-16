8d1fc2afa359a18f1a2a723ef48a5dca6e7c3e64
Sahil <sahildangol2004@gmail.com>
2024-09-16 08:33:13 +0545

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Html,useProgress } from '@react-three/drei';
+
+const Loader = () => {
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+      style={{
+        fontSize:14,
+        color:white,
+        fontWeight:800,
+        marginTop:40
+      }}
+      >{progress.toFixed(2)}%</p>
+    </Html>
+  )
+}
+
+export default Loader
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+    paddingX: "sm:px-15 lg:px-10 xs:px-10",
+    paddingY: "sm:py-5 lg:py-5 xs:py-5",
+    padding: "sm:px-15 lg:px-10 sm:py-16 py-10",
+  
+    heroHeadText:
+      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[80px] mt-3",
+    heroSubText:
+      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  
+    sectionHeadText:
+      "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    sectionSubText:
+      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+  };
+  
+  export { styles };
