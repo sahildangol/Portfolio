c85c8ddf4f7061c7c663ae0bde215e3451e70816
Sahil <sahildangol2004@gmail.com>
2024-09-19 16:42:42 +0545

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,37 +9,36 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-0'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-0">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm  <span className='text-[#915EFF]'>Sahil</span>
+            Hi, I'm <span className="text-[#915EFF]">Sahil</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-           If Not Me Who, <br className='sm:block hidden' />
-          If Not Now When?
+            SITE IS UNDER DEVELOPMENT... <br className="sm:block hidden" />
           </p>
         </div>
       </div>
 
-    <ComputersCanvas/>
+      <ComputersCanvas />
 
-      <div className='absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
-              }} 
+              }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
