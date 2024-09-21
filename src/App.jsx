c96d93af74b6ab8eccb4d1ca62692eca7a6c39b3
Sahil <sahildangol2004@gmail.com>
2024-09-21 09:24:51 +0545

    Eleventh Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import {BrowserRouter} from 'react-router-dom';
+import{About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas}from './components';
+const App=()=> {
 
-const App = () => {
   return (
-    <BrowserRouter>
-      <div className={'${styles.paddingY} relative z-50 bg-primary style'}>
-        {/* Updated div with custom background */}
-        <div className="bg-gradient-to-b from-indigo-600 to-purple-700 min-h-screen flex flex-col
-              sm:bg-gradient-to-r sm:from-blue-500 sm:to-purple-700 sm:min-h-[50vh]
-              md:bg-gradient-to-b md:from-indigo-600 md:to-purple-700 md:min-h-screen">
-          <Navbar />
-          <Hero />
-        </div>
+  <BrowserRouter>
+    <div className= {'${styles.paddingY} relative z-50 bg-primary style'}>
+    <div className="bg-gray-800 bg-hero-pattern bg-cover bg-no-repeat bg-center min-h-screen flex flex-col
+            sm:bg-contain sm:min-h-[50vh]
+            md:bg-cover md:min-h-screen">
+    <Navbar />
+    <Hero />
+</div>
+
 
         <About />
         <Experience />
@@ -19,12 +19,12 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+        <Contact />
+        <StarsCanvas />
         </div>
       </div>
-    </BrowserRouter>
-  );
-};
+  </BrowserRouter>
+  )
+}
 
 export default App;
