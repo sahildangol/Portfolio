9a9c7edf7e106b595e60bf0553cc1bed51cae310
Sahil <sahildangol2004@gmail.com>
2024-09-22 21:47:46 +0545

    fifteenth

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ const App=()=> {
   return (
   <BrowserRouter>
     <div className= {'${styles.paddingY} relative z-50 bg-primary style'}>
-    <div className="bg-gray-800 bg-hero-pattern bg-cover bg-no-repeat bg-center min-h-screen flex flex-col
-            sm:bg-contain sm:min-h-[50vh]
-            md:bg-cover md:min-h-screen">
+  
+            
     <Navbar />
     <Hero />
-</div>
+
 
 
         <About />
